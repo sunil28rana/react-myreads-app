8a689003d582da406c442b313def60aed8823ff0
Sunil Rana <sunil28rana@gmail.com>
2018-06-23 22:12:41 +0530

    feat: Add components to display 3 shelves i.e., currently reading, want to read, and read

---
+++ src/BookShelf.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Book from './Book';
+import PropTypes from 'prop-types';
+
+class BookShelf extends Component {
+  static propTypes = {
+    shelfTitle: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired
+  }
+  render() {
+    const { shelfTitle, books } = this.props;
+    return (
+
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{shelfTitle}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {
+                books.map(book =>
+                <li key={book.id}>
+                  <Book book={book}/>
+                </li>
+              )}
+            </ol>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default BookShelf;
